b780579b6feecb15422d1bb8482d1169e089dd2c
ahmedsabeh <ahmedsabeh911.com>
2025-07-07 07:38:51 +0300

    v1

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-backend-bucket-2025"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
