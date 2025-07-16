b7b51552b18ffb8984acdcadd675d596a9bd8684
ahmedsabeh <ahmedsabeh911.com>
2025-07-16 12:27:54 +0300

    v2

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,18 +1,19 @@
-variable "instance_count" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_group_id" {}
+locals {
+  instance_names = ["Jenkins-Server", "SonarQube-Server"]
+  instance_roles = ["jenkins", "sonarqube"]
+}
 
 resource "aws_instance" "app" {
-  count         = var.instance_count
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 free-tier
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  count                  = var.instance_count
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
   tags = {
-    Name = "DevOpsInstance-${count.index}"
+    Name = local.instance_names[count.index]
+    Role = local.instance_roles[count.index]
   }
 }
 
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,3 +1,25 @@
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 2
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 instances will be launched"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID for the EC2 instances"
+  type        = string
+}
+
 variable "key_name" {
   description = "Name of the AWS key pair to use for SSH access"
   type        = string
